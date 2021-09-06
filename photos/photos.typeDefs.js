518b5958bba474eed2b2f0a050693ef0a254f950
onlycan17 <onlycan17@gmail.com>
2021-09-06 20:15:30 +0900

    수업 업로드

---
+++ photos/photos.typeDefs.js
@@ -0,0 +1,27 @@
+import {gql} from "apollo-server";
+
+export default gql`
+    type Photo{
+        id: Int!
+        user: User!
+        file: String!
+        caption: String
+        hashtags: [Hashtag]
+        createAt: String!
+        updateAt: String!
+    }
+    type Hashtag {
+        id: Int!
+        hashtag: String!
+        photos(page: Int!): [Photo]
+        totalPhotos: Int!
+        createdAt: String!
+        updatedAt: String!
+    }
+    type Like {
+        id: Int!
+        photo: Photo!
+        createdAt: String!
+        updatedAt: String!
+    }
+`;
