11911362dae9bfeb21856b585f629c013d09e6cb
onlycan17 <onlycan17@gmail.com>
2021-09-28 17:33:51 +0900

    에러수정

--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -1,36 +1,39 @@
 import client from "../client";
 
 export default {
-    Photo: {
-        user:({userId}) => client.user.findUnique({where: {id: userId}}),
-        hashtags: ({id}) => 
-            client.hashtag.findMany({
-                where:{
-                    photos: {
-                        some: {
-                            id,
-                        },
-                    },
-                },
-            }),
-    },
-    Hashtag: {
-        photos: ({id}, {page}, { loggedInUser }) => {
-            return client.hashtag.findUnique({
-                where: {
-                    id,
-                },
-            }).photos();
+  Photo: {
+    user: ({ userId }) => client.user.findUnique({ where: { id: userId } }),
+    hashtags: ({ id }) =>
+      client.hashtag.findMany({
+        where: {
+          photos: {
+            some: {
+              id,
+            },
+          },
         },
-        totalPhotos: ({id}) => 
-            client.photo.count({
-                where: {
-                    hashtags: {
-                        some: {
-                            id,
-                        },
-                    },
-                },
-            }),
+      }),
+    likes: ({ id }) => client.like.count({ where: { photoId: id } }),
+  },
+  Hashtag: {
+    photos: ({ id }, { page }, { loggedInUser }) => {
+      return client.hashtag
+        .findUnique({
+          where: {
+            id,
+          },
+        })
+        .photos();
     },
+    totalPhotos: ({ id }) =>
+      client.photo.count({
+        where: {
+          hashtags: {
+            some: {
+              id,
+            },
+          },
+        },
+      }),
+  },
 };
